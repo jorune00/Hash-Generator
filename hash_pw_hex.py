553d5bbf80bdb32cf2a1b9ddab5e6a19a30b4f82
Johnny Heintz <jorune00@gmail.com>
2023-09-29 10:44:30 -0500

    Update

--- hash_pw_hex.py
+++ hash_pw_hex.py
@@ -7,9 +7,9 @@ def generate_hex_string(length=40):
     hex_chars = "02468aceACE"
     return ''.join(random.choice(hex_chars) for _ in range(length))
 
-# Generate a 40-character hash with even hex characters only
+# Generate a 40-character hash with hex characters only
 
-print("\nGenerate a 40-character hash with even hex characters only\n" +
+print("\nGenerate a 40-character hash with hex characters only\n" +
       "--------------------------------------------------------")
 length = input("Enter the length of the hash (default 40):")
 if length == "":
